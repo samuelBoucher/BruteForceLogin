ceca789ab2ba480e1dbe91ebeccb97d636e49970
Unknown <samuel.boucher@outlook.com>
2017-09-15 12:20:33 -0400

    Obtenir le nom du formulaire avec les champs spécifiés

--- __main__.py
+++ __main__.py
@@ -1,5 +1,9 @@
 from optparse import OptionParser
 from DictionaryReader import DictionaryReader
+from robobrowser import RoboBrowser
+from bs4 import BeautifulSoup
+from httplib2 import Http
+import sys
 from time import time
 
 class BruteForceLogin:
@@ -8,23 +12,21 @@ class BruteForceLogin:
     username = ''
     passname = ''
 
-    start = 0
-    end = 0
-
-
-    def execute(self):
-        self.get_args()
+    def __init__(self):
         self.print_welcome()
-        dictionary_reader = DictionaryReader(self.dict_name)
-        passwords = dictionary_reader.read()
-        for password in passwords:
-            print(password)
+        self.get_args()
+        passwords = self.read_dictionary()
+        self.brute_force_login()
+
+    def print_welcome(self):
+        print("Bienvenue à BruteForceLogin!")
 
     def get_args(self):
         parser = self.get_parser()
         (options, args) = parser.parse_args()
         self.set_attributes(options)
 
+    # En faire une classe
     def get_parser(self):
         parser = OptionParser()
         parser.add_option("-r", "--url", dest="url")
@@ -39,10 +41,37 @@ class BruteForceLogin:
         self.username = options.username
         self.passname = options.passname
 
-    def print_welcome(self):
-        print("Bienvenue à BruteForceLogin!")
+    def read_dictionary(self):
+        dictionary_reader = DictionaryReader(self.dict_name)
+        passwords = dictionary_reader.read()
+        return passwords
+
+    # En faire une classe
+    def brute_force_login(self):
+        browser = RoboBrowser(history=True)
+        browser.open(self.url)
+        all_forms = browser.get_forms()
+        form_name = self.get_form_name()
+
+    def get_form_name(self):
+        http = Http('.cache')
+        response, content = http.request('http://altoromutual.com/bank/login.aspx')
+        forms = BeautifulSoup(content, "html.parser").findAll('form')
+
+        for form in forms:
+            str_form = form.prettify()
+            if self.username in str_form and self.passname in str_form:
+                str_after_name = str_form.split("name=\"", 1)[1]  # ex: <form name="uid" id="id">
+                                                                 # devient
+                                                                 #  uid" id="id">
+
+                return str_after_name.split("\"", 1)[0]  # ex: uid" id="id">
+                                                         # devient
+                                                         # uid
+
+        print("Le formulaire avec les noms de champs spécifiés sont introuvables, fermeture de l'application")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
     brute_force_login = BruteForceLogin()
-    brute_force_login.execute()
